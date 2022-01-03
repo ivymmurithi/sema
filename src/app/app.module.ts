194113dd4fe630081cba50b86d487a2f1cfb9ddf
ivymmurithi <ivymurithi@gmail.com>
2022-01-03 20:11:42 +0300

    add display quote component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputFormComponent } from './input-form/input-form.component';
+import { DisplayQuoteComponent } from './display-quote/display-quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputFormComponent
+    InputFormComponent,
+    DisplayQuoteComponent
   ],
   imports: [
     BrowserModule,
