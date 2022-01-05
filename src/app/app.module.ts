c08b89502579463cc5268faf4295e091ac7e6680
ivymmurithi <ivymurithi@gmail.com>
2022-01-05 12:48:18 +0300

    generate custom date pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { InputFormComponent } from './input-form/input-form.component';
 import { DisplayQuoteComponent } from './display-quote/display-quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { DatePostedPipe } from './pipes/date-posted.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputFormComponent,
     DisplayQuoteComponent,
-    QuoteDetailsComponent
+    QuoteDetailsComponent,
+    DatePostedPipe
   ],
   imports: [
     BrowserModule,
