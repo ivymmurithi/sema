c29c6624350fc42c94e69e57d5d527925cfc91cf
ivymmurithi <ivymurithi@gmail.com>
2022-01-05 12:09:27 +0300

    upvote counts to increment

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-quote-details',
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+
+  count = 0;
+
+  addUpvote() {
+    this.count++;
+    console.log(this.count);
+  }
 
   constructor() { }
 
